ede1177d71e67face1b1348d93fe48fceb605d4a
Porfirio Quintero <porfirioqc@gmail.com>
2017-10-20 17:31:07 -0700

    aws103 dir

--- bebi103_AWSconfig.sh
+++ bebi103_AWSconfig.sh
@@ -1,3 +1,8 @@
+# Configure an AWS EC2 server for bebi103
+# Install miniconda with relevant python packages
+# and setup Jupyter Notebooks
+# Author: Porfirio Quintero
+
 # Download and install conda
 echo 'Getting miniconda...'
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
@@ -6,7 +11,11 @@ bash ./miniconda.sh -b -p $HOME/miniconda;
 export PATH="$HOME/miniconda/bin:$PATH"
 # Add to PATH and PYTHONPATH in .bashrc
 echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
-echo 'export PYTHONPATH=:.:$HOME/bebi103_utils:$PYTHONPATH' >> ~/.bashrc
+# Add bebi103_utils to PYTHONPATH
+echo 'export PYTHONPATH=:.:$HOME/aws103:$PYTHONPATH' >> ~/.bashrc
+
+# Update bashrc
+source ~/.bashrc
 
 conda config --set always_yes yes --set changeps1 no;
 conda update -q conda;
@@ -17,24 +26,26 @@ echo 'Getting your python packages...'
 conda install -y numpy scipy pandas pymc3 numba tqdm scikit-image
 conda install -yc astropy emcee
 # plotting
-conda install -y matplotlib seaborn bokeh holoviews jupyter
+conda install -y matplotlib seaborn bokeh holoviews jupyter datashader
 
-# Needed for bebi103 to work
+# install bebi103_utils
+pip install bebi103
+# Needed for bebi103_utils to work
 sudo yum -y install mesa-libGL.x86_64
-
-# Create directories
-mkdir ~/certs ~/notebooks ~/notebooks/data ~/notebooks/output
 
 # Generate jupyter configuration file and certificate
 jupyter notebook --generate-config
 echo 'Please complete your certificate details'
+mkdir ~/certs
 cd ~/certs
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout mycert.pem -out mycert.pem
 
 # Create and save jupyter password
-python ~/bebi103_utils/jupyter_passconfig.py
+python ~/aws103/jupyter_passconfig.py
 
 # Download datasets
 echo 'Downloading the bebi103 datasets...'
-cd ~/notebooks/data
+cd ~/*bebi103
+mkdir ./data
+cd ./data
 # wget?
